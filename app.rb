a7230d7c1ae7c612e6b873b00205b4ffe43e1056
kateleext <83829378+kateleext@users.noreply.github.com>
2025-01-28 22:04:44 +0000

    project complete

--- app.rb
+++ app.rb
@@ -2,8 +2,44 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/rock") do
+  @ourmove = "rock"
+  @theirmove = ["rock","paper","scissors"][rand(3)]
+  if @theirmove == "paper"
+    @outcome = "lost"
+  elsif @theirmove == "rock"
+    @outcome = "tied"
+  elsif @theirmove == "scissors"
+    @outcome = "won"
+  end
+  erb(:play)
+end
+
+get("/paper") do
+  @ourmove = "paper"
+  @theirmove = ["rock","paper","scissors"][rand(3)]
+  if @theirmove == "paper"
+    @outcome = "tied"
+  elsif @theirmove == "rock"
+    @outcome = "won"
+  elsif @theirmove == "scissors"
+    @outcome = "lost"
+  end
+  erb(:play)
+end
+
+get("/scissors") do
+  @ourmove = "scissors"
+  @theirmove = ["rock","paper","scissors"][rand(3)]
+  if @theirmove == "paper"
+    @outcome = "won"
+  elsif @theirmove == "rock"
+    @outcome = "lost"
+  elsif @theirmove == "scissors"
+    @outcome = "tied"
+  end
+  erb(:play)
 end
